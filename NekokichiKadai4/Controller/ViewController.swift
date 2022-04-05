8dae1aff6d0d64ec79ff86326d8e53547b965449
CatLuck2 <freeaddress834@gmail.com>
2022-04-05 14:49:04 +0900

    必要な機能は完成

--- NekokichiKadai4/Controller/ViewController.swift
+++ NekokichiKadai4/Controller/ViewController.swift
@@ -10,12 +10,21 @@ import UIKit
 final class ViewController: UIViewController {
 
     @IBOutlet weak private var incrementedNumLabel: UILabel!
+    private var number: CountUp = Number()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        incrementedNumLabel.text = "\(number.num)"
+    }
 
     @IBAction func incrementNumButton(_ sender: UIButton) {
-
+        number.increment()
+        incrementedNumLabel.text = "\(number.num)"
     }
 
     @IBAction func clearIncrementedNumButton(_ sender: UIButton) {
+        number.reset()
+        incrementedNumLabel.text = "\(number.num)"
     }
 
 }
--- NekokichiKadai4/Model/Number.swift
+++ NekokichiKadai4/Model/Number.swift
@@ -7,14 +7,14 @@
 
 import UIKit
 
-private protocol CountUp {
+protocol CountUp {
     var num: Int { get set }
     mutating func increment()
     mutating func reset()
 }
 
 struct Number: CountUp {
-    var num: Int
+    var num: Int = 0
 
     mutating func increment() {
         num += 1
