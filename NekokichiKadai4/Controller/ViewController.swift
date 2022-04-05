07b2a80374725a4ff0747c836818f48cbf8cab09
CatLuck2 <freeaddress834@gmail.com>
2022-04-05 15:30:00 +0900

    メソッド名を変更

--- NekokichiKadai4/Controller/ViewController.swift
+++ NekokichiKadai4/Controller/ViewController.swift
@@ -14,25 +14,25 @@ final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setNumInIncrementedNumLabel(countedUpNumber.num)
+        setNumToIncrementedNumLabel(countedUpNumber.num)
     }
 
     /*
      ・Labelの値を3度（初期表示、カウントアップ、初期化）変更するので、
-     代入処理を関数で実装
+        代入処理を関数で実装
      */
-    private func setNumInIncrementedNumLabel(_ value: Int) {
+    private func setNumToIncrementedNumLabel(_ value: Int) {
         incrementedNumLabel.text = "\(value)"
     }
 
     @IBAction func incrementNumButton(_ sender: UIButton) {
         countedUpNumber.increment()
-        setNumInIncrementedNumLabel(countedUpNumber.num)
+        setNumToIncrementedNumLabel(countedUpNumber.num)
     }
 
     @IBAction func clearIncrementedNumButton(_ sender: UIButton) {
         countedUpNumber.reset()
-        setNumInIncrementedNumLabel(countedUpNumber.num)
+        setNumToIncrementedNumLabel(countedUpNumber.num)
     }
 
 }
